aaddaa69ad3c92d47b0295c82c5c8d82bd0dd3aa
Rami Serapian <rsn@Ramis-MacBook-Pro.local>
2020-06-27 21:05:23 -0400

    First commit: Show products in table

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  sky: string;
+  name: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: boolean;
+  unitsInStock: number;
+  dateCreated: Date;
+  lastUpdated: Date;
+}
